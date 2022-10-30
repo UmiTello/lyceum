7ffeebaab7e0cf79b0074023b6200149633e4876
Mukhammadumid Toshmamatov <mamidus@Mukhammadumids-MacBook-Pro.local>
2022-10-31 01:40:03 +0500

    form fixed

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -4,7 +4,7 @@ function passContact(e) {
 	let text = document.getElementById("contact_message").value.replace(/ /g, "%20");
 
 	var TOKEN = "5766249030:AAHXicdKFj9I0O5Cfrn2HO3LESos0aSRc0Q";
-	var chatID = "-1001422474562";
+	var chatID = "-890044742";
 	var api = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + chatID + "&parse_mode=html&text=";
 
 	var message2 = `Bog'lanish formasidan yangi xabar: %0A%0A<b>Xabar jo'natuvchi ismi: </b> ${name} %0A<b>Xabar jo'natuvchi telefon raqami: </b> ${phone} %0A<b>Xabarning mazmuni: </b> ${text} %0A`;
